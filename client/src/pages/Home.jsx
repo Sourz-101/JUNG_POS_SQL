d752f5df8f6254b483c0a8f27ef8a56af69bc4cb
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-05-31 16:00:11 +0530

    added loader in serch bar and no product matches tag

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { IoAddOutline } from "react-icons/io5";
 const Home = () => {
   const [text, setText] = useState("");
   const [style, setStyle] = useState("center");
-
+  const [loading, setLoading]=useState(false)
   const [resustantData, setResustantData] = useState([]);
   const fetchProdcuts = async () => {
     try {
@@ -30,11 +30,14 @@ const Home = () => {
       setResustantData(resData);
     } catch (error) {
       console.log(error);
+    }finally{
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     if (text === "") {
+
       setResustantData([]);
       setStyle("center");
       document.getElementById("input").style.width = "400px";
@@ -46,37 +49,49 @@ const Home = () => {
       document.getElementById("input").style.width = "80%";
       document.getElementById("cont").style.justifyContent = "start";
     }
-
+    setLoading(true);
     let timmer = setTimeout(() => {
       console.log("fetchiin....");
       fetchProdcuts();
     }, 500);
 
     return () => {
+      setLoading(false);
       clearTimeout(timmer);
+
     };
   }, [text]);
   return (
     <div
-      className={`h-screen bg-[#F9FDFF] w-full flex flex-col gap-10 items-center text-4xl`}
+      className={`h-screen bg-[#F9FDFF] w-full flex flex-col gap-10 items-center text-4xl z-0`}
       id="cont"
       onClick={() => setText("")}
-    >
-    {text==='' ? <Link to={`/addproduct`} className="fixed flex top-10 right-10 text-xl font-semibold bg-[#1470EF] cursor-pointer hover:bg-blue-700 text-white p-2 rounded-lg"> <IoAddOutline size={30}/> Add Product</Link> : ""}
+    > 
+      {text === "" ? (
+        <Link
+          to={`/addproduct`}
+          className="fixed flex top-10 right-10 text-xl font-semibold bg-[#1470EF] cursor-pointer hover:bg-blue-700 text-white p-2 rounded-lg"
+        >
+          {" "}
+          <IoAddOutline size={30} /> Add Product
+        </Link>
+      ) : (
+        ""
+      )}
       {/* <SearchBar/> */}
-      {
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          className="transition-all duration-500 m-5 p-2 border-2 border-[#1849A8] rounded-lg w-60 text-black text-center z-5"
-          id="input"
-          placeholder="Search Product"
-        />
-      }
+
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="transition-all z-40 duration-500 m-5 p-2 border-2 border-[#1849A8] rounded-lg w-60 text-black text-center"
+        id="input"
+        placeholder="Search Product"
+      />
+
       {text != "" ? (
-        <div className="result relative shadow-sm shadow-black w-[1100px]  rounded-lg overflow-y-auto overflow-x-hidden top-14">
-          {resustantData?.map((e) => {
+        <div className="result relative shadow-sm shadow-black w-[80vw]  rounded-lg overflow-y-auto overflow-x-hidden -top-14">
+          { resustantData.length  ? resustantData?.map((e) => {
             return (
               <Link to={`/product/${e.prod_id}`} key={e.prod_id}>
                 <div className="w-full bg-white rounded-sm flex justify-around items-center p-2 border-2 cursor-pointer hover:bg-slate-100">
@@ -92,7 +107,9 @@ const Home = () => {
                 </div>
               </Link>
             );
-          })}
+          }) : <div className="w-full bg-white rounded-sm flex justify-around items-center p-2 border-2 cursor-pointer hover:bg-slate-100">
+                 {!loading ? "No Related Product Found!" : <span className="loading loading-bars loading-sm"></span>} 
+                </div>}
         </div>
       ) : (
         ""
